2193c406fc578af0cb2b49b6c8caece5cf6553b8
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2022-06-14 21:50:28 -0300

    add negative integer and zero cases

--- backend/__tests__/add.spec.js
+++ backend/__tests__/add.spec.js
@@ -5,4 +5,14 @@ describe("Addition tests", () => {
         result = add(1, 1)
         expect(result).toBe(2)
     })
+
+    test("-5 + 1 should be -4", () => {
+        result = add(-5, 1)
+        expect(result).toBe(-4)
+    })
+
+    test("0 + 10 should be 10", () => {
+        result = add(0, 10)
+        expect(result).toBe(10)
+    })
 })
