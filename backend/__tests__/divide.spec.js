fd0132c97f6909d4eb75d1221e9924993512d1a5
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2022-06-15 00:37:39 -0300

    refactor to use toEqual matcher

--- backend/__tests__/divide.spec.js
+++ backend/__tests__/divide.spec.js
@@ -3,21 +3,21 @@ const { divide } = require("../src/divide/divide")
 describe("Division tests", () => {
     test("10 / 2 should be 5", () => {
         result = divide(10, 2)
-        expect(result).toBe(5)
+        expect(result).toEqual(5)
     })
 
     test("-13 / 1 should be -13", () => {
         result = divide(-13, 1)
-        expect(result).toBe(-13)
+        expect(result).toEqual(-13)
     })
 
     test("-6 / -12 should be 0.5", () => {
         result = divide(-6, -12)
-        expect(result).toBe(0.5)
+        expect(result).toEqual(0.5)
     })
 
     test("5 / 14 should be 0.357", () => {
         result = divide(5, 14)
-        expect(result).toBe(0.357)
+        expect(result).toEqual(0.357)
     })
 })
