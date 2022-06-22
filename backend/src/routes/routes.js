4b063ffd704c6448b47b522344979f179c9df350
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2022-06-21 21:55:28 -0300

    implement multiply endpoint and add API tests

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -4,11 +4,13 @@ const mathRouter = new Router()
 
 const { addRouter } = require("./math/add.route")
 const { subtractRouter } = require("./math/subtract.route")
+const { multiplyRouter } = require("./math/multiply.route")
 
 mathRouter.prefix("/math")
 mathRouter
     .use(addRouter.routes(), addRouter.allowedMethods())
     .use(subtractRouter.routes(), subtractRouter.allowedMethods())
+    .use(multiplyRouter.routes(), multiplyRouter.allowedMethods())
 
 mainRouter.use(mathRouter.routes())
 
