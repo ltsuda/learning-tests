16d2d028d2caeabd93c1b3a4ca3172d18e6eaa01
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2022-06-21 21:42:20 -0300

    implement subtract endpoint and add API tests

---
+++ backend/__tests__/api/subtract.route.spec.js
@@ -0,0 +1,37 @@
+const { test, expect } = require("@playwright/test")
+
+test("512 - 256 should be 256", async ({ request }) => {
+    const response = await request.post("/math/subtract", { params: { operand1: 512, operand2: 256 } })
+    expect(response.status()).toEqual(200)
+    expect(await response.json()).toEqual({
+        message: "OK",
+        result: 256,
+    })
+})
+
+test("-128 - -128 should be 0", async ({ request }) => {
+    const response = await request.post("/math/subtract", { params: { operand1: -128, operand2: -128 } })
+    expect(response.status()).toEqual(200)
+    expect(await response.json()).toEqual({
+        message: "OK",
+        result: 0,
+    })
+})
+
+test("5 + 'def' should result in 400", async ({ request }) => {
+    const response = await request.post("/math/subtract", { params: { operand1: 5, operand2: "def" } })
+    expect(response.status()).toEqual(400)
+    expect(await response.json()).toEqual({
+        message: "Operands are not defined or are not a number",
+        operands: { operand1: "5", operand2: "def" },
+    })
+})
+
+test("Missing input should result in 400", async ({ request }) => {
+    const response = await request.post("/math/subtract", { params: { operand1: 100 } })
+    expect(response.status()).toEqual(400)
+    expect(await response.json()).toEqual({
+        message: "Operands are not defined or are not a number",
+        operands: { operand1: "100", operand2: null },
+    })
+})
--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -3,9 +3,12 @@ const mainRouter = new Router()
 const mathRouter = new Router()
 
 const { addRouter } = require("./math/add.route")
+const { subtractRouter } = require("./math/subtract.route")
 
 mathRouter.prefix("/math")
-mathRouter.use(addRouter.routes(), addRouter.allowedMethods())
+mathRouter
+    .use(addRouter.routes(), addRouter.allowedMethods())
+    .use(subtractRouter.routes(), subtractRouter.allowedMethods())
 
 mainRouter.use(mathRouter.routes())
 
