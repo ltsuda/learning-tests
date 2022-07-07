3ff883e4d258c3cbb002cde139fb2b3a92a8a563
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2022-07-06 23:57:03 -0300

    add Navbar component to all route pages

---
+++ frontend/src/routes/math/index.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { activeNavItem } from "../../components/stores"
+    activeNavItem.set("math")
+    import Navbar from "../../components/navbar/Navbar.svelte"
+</script>
+
+<Navbar/>
