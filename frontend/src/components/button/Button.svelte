7aad075f789061a818759388f7e8a6cd70a74f0a
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2022-07-15 23:21:04 -0300

    implement initial License's page design

---
+++ frontend/src/components/button/Button.svelte
@@ -0,0 +1,33 @@
+<div class="button-container" data-id="button-container">
+    <button type="button" class="button-submit" data-id="button-submit">Submit</button>
+</div>
+
+<style>
+    .button-container {
+        margin: 0;
+        max-width: 200px;
+        min-width: 160px;
+        width: 200px;
+        flex: 20%;
+    }
+    button {
+        width: 100%;
+        height: 64px;
+        background: #c9ada7;
+        text-transform: uppercase;
+        color: white;
+        font-weight: bold;
+        letter-spacing: 1px;
+        border: none;
+        font-size: 18px;
+        /* font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif; */
+        cursor: pointer;
+    }
+    button:hover,
+    button:focus {
+        background-color: #4a4e69;
+    }
+    button:disabled {
+        background-color: #9a8c98;
+    }
+</style>
--- frontend/src/routes/license/index.svelte
+++ frontend/src/routes/license/index.svelte
@@ -1,4 +1,29 @@
 <script>
+    import "./license.css"
     import { activeNavItem } from "../../components/stores"
     activeNavItem.set("license")
+    import Button from "../../components/button/Button.svelte"
 </script>
+
+<div class="svelte-content" data-id="svelte-content">
+    <div class="question-container" data-id="question-container">
+        <p>What is your age?</p>
+    </div>
+    <div class="input-age-container" data-id="input-age-container">
+        <input
+            type="text"
+            placeholder="Age"
+            class="input-age"
+            data-id="input-age"
+            maxlength="3"
+            min="1"
+            max="150"
+            required
+        />
+    </div>
+    <Button />
+
+    <div class="answer-container" data-id="answer-container">
+        <p>Here is the response</p>
+    </div>
+</div>
