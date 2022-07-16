278b7823f948457e1736ce6ef8cbca6e3b12bc6c
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2022-07-15 21:30:05 -0300

    add welcome svelte elements into flex item 2

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -3,5 +3,7 @@
     activeNavItem.set("")
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="svelte-content" data-id="svelte-content">
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
