fd5f5f7dae298da803646903706f7291c6de1be9
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2022-07-20 00:13:23 -0300

    implement initial math page design

---
+++ frontend/src/components/select-operation/select.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let class_name
+    export let data_id
+    let selected = "addition"
+    export let title = "operations"
+    let operations = [
+        { data_id: "addition", value: "addition", text: "\u002b" },
+        { data_id: "subtraction", value: "subtraction", text: "\u2212" },
+        { data_id: "multiplication", value: "multiplication", text: "\u00d7" },
+        { data_id: "division", value: "division", text: "\u00f7" },
+    ]
+</script>
+
+<select class={class_name} data-id={data_id} {title} bind:value={selected} required>
+    {#each operations as operation}
+        <option value={operation.value} data-id={operation.data_id}>
+            {operation.text}
+        </option>
+    {/each}
+</select>
+
+<style>
+    select {
+        cursor: pointer;
+        -webkit-appearance: none;
+    }
+</style>
--- frontend/src/routes/math/index.svelte
+++ frontend/src/routes/math/index.svelte
@@ -1,4 +1,27 @@
 <script>
+    import "./math.css"
     import { activeNavItem } from "../../components/stores"
     activeNavItem.set("math")
+    import Select from "../../components/select-operation/select.svelte"
+    import Button from "../../components/button/Button.svelte"
+    import Input from "../../components/input/input.svelte"
+    let inputPattern = "-?[0-9]{1,4}"
 </script>
+
+<div class="svelte-content" data-id="svelte-content">
+    <div class="instruction-container" data-id="instruction-container">
+        <p class="instruction-title">Calculator</p>
+        <p class="instruction-description">(only four digits operands)</p>
+    </div>
+    <div class="select-operation-container">
+        <Input class_name="first-operand" data_id="first-operand" placeholder="Operand 1" pattern={inputPattern} />
+        <Select class_name="select-operation" data_id="select-operation" />
+        <Input class_name="second-operand" data_id="second-operand" placeholder="Operand 2" maxlength="255" />
+    </div>
+    <div class="button-container" data-id="button-container">
+        <Button />
+    </div>
+    <div class="answer-container" data-id="answer-container">
+        <p />
+    </div>
+</div>
